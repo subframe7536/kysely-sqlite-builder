4889094cda0f4f1b5dc18441a4e0df6fda706a6f
subframe7536 <1667077010@qq.com>
2024-03-26 11:32:33 +0800

    feat: improve createSqliteBuilder

--- src/create.ts
+++ src/create.ts
@@ -3,11 +3,31 @@ import { type InferDatabase, type Schema, useSchema } from './schema'
 import type { SyncOptions } from './schema/core'
 import type { SqliteBuilderOptions } from './builder'
 
+export type CreateSqliteBuilderOptions<
+  T extends Schema,
+  Extra extends Record<string, any>,
+> = SqliteBuilderOptions<InferDatabase<T>, Extra> & {
+  /**
+   * {@link Schema}
+   */
+  schema: T
+  /**
+   * {@link SyncOptions}
+   */
+  sync?: SyncOptions<T>
+  /**
+   * whether to check integrity
+   */
+  checkIntegrity?: boolean
+}
+
+/**
+ * {@link SqliteBuilder}
+ */
 export async function createSqliteBuilder<T extends Schema, Extra extends Record<string, any>>(
-  schema: T,
-  options: SqliteBuilderOptions<InferDatabase<T>, Extra> & { sync?: SyncOptions<T>, checkIntegrity?: boolean },
+  options: CreateSqliteBuilderOptions<T, Extra>,
 ): Promise<Omit<SqliteBuilder<InferDatabase<T>, Extra>, 'syncDB'>> {
   const builder = new SqliteBuilder(options)
-  await builder.syncDB(useSchema(schema, options.sync), options.checkIntegrity)
+  await builder.syncDB(useSchema(options.schema, options.sync), options.checkIntegrity)
   return builder
 }
