0f0ffc9cb1bdc39afe1f6bc1dec1ba0187e90894
subframe7536 <1667077010@qq.com>
2024-09-11 09:27:46 +0800

    chore: use tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: {
@@ -7,7 +7,6 @@ export default defineConfig({
     migrator: 'src/migrator/index.ts',
   },
   clean: true,
-  external: ['unplugin'],
   format: ['cjs', 'esm'],
   dts: true,
   treeshake: true,
