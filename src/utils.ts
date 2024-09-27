c5232811f7219e6b0543bdaa2e2c983954c58198
subframe7536 <1667077010@qq.com>
2024-09-27 12:38:33 +0800

    feat: executeSQL support more types

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,32 @@
-import { CompiledQuery, type Kysely, type QueryResult, type RawBuilder } from 'kysely'
+import {
+  CompiledQuery,
+  type DatabaseConnection,
+  type Kysely,
+  type QueryResult,
+  type Transaction,
+} from 'kysely'
 
-export async function executeSQL<O>(kysely: Kysely<any>, rawSql: RawBuilder<O>): Promise<QueryResult<O>>
-/**
- * Execute sql string
- */
-export async function executeSQL<O>(kysely: Kysely<any>, rawSql: string, parameters?: unknown[]): Promise<QueryResult<O>>
+export type KyselyInstance = DatabaseConnection | Kysely<any> | Transaction<any>
+
 /**
  * Execute compiled query and return result list
  */
-export async function executeSQL<O>(kysely: Kysely<any>, query: CompiledQuery<O>): Promise<QueryResult<O>>
+export async function executeSQL<O>(kysely: KyselyInstance, query: CompiledQuery<O>): Promise<QueryResult<O>>
+/**
+ * Execute sql string
+ */
 export async function executeSQL<O>(
-  kysely: Kysely<any>,
-  data: CompiledQuery<O> | RawBuilder<O> | string,
+  kysely: KyselyInstance,
+  rawSql: string,
+  parameters?: unknown[]
+): Promise<QueryResult<O>>
+export async function executeSQL<O>(
+  kysely: KyselyInstance,
+  data: CompiledQuery<O> | string,
   parameters?: unknown[],
 ): Promise<QueryResult<O>> {
   if (typeof data === 'string') {
-    return await kysely.executeQuery<O>(CompiledQuery.raw(data, parameters))
-  } else if ('sql' in data) {
-    return await kysely.executeQuery<O>(data)
-  } else {
-    return await data.execute(kysely)
+    data = CompiledQuery.raw(data, parameters)
   }
+  return await kysely.executeQuery(data)
 }
