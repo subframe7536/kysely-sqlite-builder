e53890972b8fe62359724bfb19c5fe2ee6022c2d
subframe7536 <1667077010@qq.com>
2024-10-11 00:00:18 +0800

    wip: index

--- src/schema/parse-exist.ts
+++ src/schema/parse-exist.ts
@@ -1,14 +1,17 @@
-import type { ParsedColumnType, Table } from './types'
+import type { ColumnProperty, ParsedColumnType, Table } from './types'
 import { type QueryCreator, sql } from 'kysely'
 
 export type ParseExistSchemaExecutor = Pick<QueryCreator<any>, 'selectFrom'>
+export type PragmaIndexList = {
 
+}
 export type ParsedSchema = {
   table: ParsedTables
   index: [name: string, table: string][]
   trigger: string[]
 }
 type ParsedTables = Record<string, ParsedTableInfo>
+type ParsedColumns = Record<string, ColumnProperty>
 export type ParsedTableInfo = Required<Omit<Table, 'softDelete' | 'timeTrigger'>> & {
   primary: string[]
   trigger: string[]
@@ -23,38 +26,33 @@ export type ParsedColumnProperty = {
  * parse table object
  * @param db kysely instance
  * @param tableName table name
- * @todo support extra constraints
  */
-export async function parseTable(db: ParseExistSchemaExecutor, tableName: string): Promise<ParsedTableInfo> {
-  /// keep-sorted
-  const result: ParsedTableInfo = {
-    columns: {},
-    index: [],
-    primary: [],
-    trigger: [],
-    unique: [],
-  }
-
+export async function parseTable(db: ParseExistSchemaExecutor, tableName: string): Promise<[columns: ParsedColumns, primaryKey: string[]]> {
+  const columns: ParsedColumns = {}
+  const pk: string[] = []
   const cols = await db
     .selectFrom(sql`pragma_table_info(${tableName})`.as('t'))
     .select(['name', 'type', 'notnull', 'dflt_value', 'pk'])
     .execute()
-  const indexes = await db
-    .selectFrom(sql`pragma_index_list(${tableName})`.as('i'))
-    .select(['name', 'unique', 'origin'])
-    .execute()
   for (const { dflt_value, name, notnull, pk, type } of cols) {
-    result.columns[name] = {
+    columns[name] = {
       type,
       notNull: !!notnull as any,
       defaultTo: dflt_value,
     }
     if (pk) {
-      result.primary.push(name)
+      pk.push(name)
     }
   }
 
-  return result
+  return [columns, pk]
+}
+
+export async function parseIndex(db: ParseExistSchemaExecutor, indexName: string) {
+  const indexes = await db
+    .selectFrom(sql`pragma_index_list(${indexName})`.as('i'))
+    .select(['name', 'unique', 'origin'])
+    .execute()
 }
 
 /**
@@ -67,13 +65,16 @@ export async function parseExistSchema(
   const tables = await db
     .selectFrom('sqlite_master')
     .where('type', 'in', ['table', 'trigger', 'index'])
-    .where('name', 'not like', 'sqlite_%')
-    .orderBy('type', 'desc')
+    .where(qb => qb.or([
+      qb('name', 'not like', 'sqlite_%'),
+      qb('name', 'like', 'sqlite_autoindex%'),
+    ]))
     .$if(prefix.length > 0, qb => qb.where(
       eb => eb.and(
         prefix.map(t => eb('name', 'not like', `${t}%`)),
       ),
     ))
+    .orderBy('type', 'desc')
     .select(['name', 'type', 'tbl_name', 'sql'])
     .execute()
 
