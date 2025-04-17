01ef00b5116e1261cc5426a71529197811078b9a
subframe7536 <1667077010@qq.com>
2025-04-17 13:24:48 +0800

    fix: fix dts `inputAlias`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,28 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import { defineConfig } from 'tsdown'
 
+const dir = path.dirname(fileURLToPath(import.meta.url))
+
+const entry = {
+  index: 'src/index.ts',
+  schema: 'src/schema/index.ts',
+  migrator: 'src/migrator/index.ts',
+}
+
+const inputAlias = Object.fromEntries(
+  Object.entries(entry)
+    .map(([k, v]) => [path.join(dir, v).replace('.ts', '.d.ts'), `${k}.d`]),
+)
+
 export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    schema: 'src/schema/index.ts',
-    migrator: 'src/migrator/index.ts',
-  },
+  entry,
   clean: true,
   format: ['cjs', 'esm'],
   dts: {
     isolatedDeclaration: true,
+    inputAlias,
   },
   treeshake: true,
 })
