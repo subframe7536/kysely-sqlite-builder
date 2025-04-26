8e9535fc51bbc0eeee9b698dbde02fe7d0ce8216
Song <1667077010@qq.com>
2025-04-26 09:36:20 +0000

    tsdown@0.9.8

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,23 +1,14 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'tsdown'
 
-const dir = path.dirname(fileURLToPath(import.meta.url))
-
-const entry = {
-  index: 'src/index.ts',
-  schema: 'src/schema/index.ts',
-  migrator: 'src/migrator/index.ts',
-}
-
 export default defineConfig({
-  entry,
+  entry: {
+    index: 'src/index.ts',
+    schema: 'src/schema/index.ts',
+    migrator: 'src/migrator/index.ts',
+  },
   clean: true,
   format: ['cjs', 'esm'],
-  external: ['@subframe7536/type-utils'],
   dts: {
     isolatedDeclarations: true,
   },
-  treeshake: true,
 })
