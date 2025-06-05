a805df5b4d9535de8e1ba54b627446cbbf76e317
Song <1667077010@qq.com>
2025-06-05 14:34:38 +0000

    upgrade deps

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'bunup'
 
 export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    schema: 'src/schema/index.ts',
-    migrator: 'src/migrator/index.ts',
-  },
+  entry: ['src/index.ts','src/schema/index.ts','src/migrator/index.ts'],
   clean: true,
   format: ['cjs', 'esm'],
-  dts: true,
+  // dts: true,
+  dts: false,
 })
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: {
-    index: 'src/index.ts',
-    schema: 'src/schema/index.ts',
-    migrator: 'src/migrator/index.ts',
-  },
-  clean: true,
+  // entry: {
+  //   index: 'src/index.ts',
+  //   schema: 'src/schema/index.ts',
+  //   migrator: 'src/migrator/index.ts',
+  // },
+  entry: [
+     'src/index.ts',
+     'src/schema/index.ts',
+     'src/migrator/index.ts',
+  ],
+  // clean: true,
   format: ['cjs', 'esm'],
-  dts: {
-    isolatedDeclarations: true,
-  },
-  exports: true
+  // dts: {
+  //   isolatedDeclarations: true,
+  // },
+  dts: false,
+  // exports: true
 })
